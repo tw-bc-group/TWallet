035ab3cde238279809d13596a66b79ea56839341
zyzhao <zyzhao@thoughtworks.com>
2020-04-14 15:14:18 +0800

    feat: recover home page dialog logic

--- lib/views/home/assets/assets_page.dart
+++ lib/views/home/assets/assets_page.dart
@@ -2,12 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:optional/optional_internal.dart';
+import 'package:provider/provider.dart';
+import 'package:tw_wallet_ui/global/common/application.dart';
 import 'package:tw_wallet_ui/global/common/get_it.dart';
 import 'package:tw_wallet_ui/global/common/theme.dart';
 import 'package:tw_wallet_ui/global/store/identity_store.dart';
 import 'package:tw_wallet_ui/models/identity.dart';
 import 'package:tw_wallet_ui/views/home/assets/point_tab.dart';
 import 'package:tw_wallet_ui/views/home/assets/token_tab.dart';
+import 'package:tw_wallet_ui/views/home/home.dart';
+import 'package:tw_wallet_ui/views/home/home_store.dart';
+import 'package:tw_wallet_ui/widgets/button.dart';
 
 class AssetsPage extends StatefulWidget {
   const AssetsPage();
@@ -30,6 +35,45 @@ class _AssetsPageState extends State<AssetsPage>
   void initState() {
     super.initState();
     _tabController = TabController(length: _tabs.length, vsync: this);
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await _showAddIdentityDialog();
+    });
+  }
+
+  _showAddIdentityDialog() async {
+    var identities = _store.identities;
+    if (identities.isEmpty) {
+      await showDialog<String>(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) => new SimpleDialog(
+          contentPadding: EdgeInsets.symmetric(horizontal: 50, vertical: 25),
+          children: <Widget>[
+            Center(
+                child: Text(
+              '您还没有添加身份，请前往\"身份\"页面添加身份',
+              style: TextStyle(
+                fontSize: 15,
+                height: 1.82,
+              ),
+              textAlign: TextAlign.center,
+            )),
+            Container(
+                margin: EdgeInsets.only(top: 48),
+                child: Button(
+                  width: 100,
+                  height: 40,
+                  text: '确定',
+                  onPressed: () {
+                    Application.router.pop(context);
+                    Provider.of<HomeStore>(context, listen: false)
+                        .changePage(HomeState.identityIndex);
+                  },
+                ))
+          ],
+        ),
+      );
+    }
   }
 
   Widget buildHeader(
