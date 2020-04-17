ddfe56c1a30c5c382bef5e073c36b38223b76a01
xudongdong <dongdong.xu1@thoughtworks.com>
2020-04-17 15:37:19 +0800

    fix: fix ci

--- lib/views/tx_list/tx_list_page.dart
+++ lib/views/tx_list/tx_list_page.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tw_wallet_ui/global/common/theme.dart';
-import 'package:tw_wallet_ui/views/tx_list/color_money.dart';
 
 class TxListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: WalletTheme.mainBgColor,
-      body: Container(child: ColorMoney()),
     );
   }
 }
