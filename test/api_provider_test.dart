70a39975ac04ac803d1206da61a6cedd03ab3907
xudongdong <dongdong.xu1@thoughtworks.com>
2020-04-21 17:24:50 +0800

    feat: add integration test #10

---
+++ test/api_provider_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tw_wallet_ui/global/service/api_provider.dart';
+import 'package:tw_wallet_ui/models/transaction.dart';
+
+void main() {
+  test("query tx list", () async {
+    final ApiProvider provider = new ApiProvider();
+
+    List<Transaction> list;
+    try {
+      list =
+          await provider.listTx("0xed9d02e382b34818e88B88a309c7fe71E65f419d");
+      print(list.first.hash);
+    } catch (e) {
+      print(e);
+    }
+
+    expect(list.length, equals(1));
+  });
+}
