4fa4bd7ed64ff2bc1e883e4a24080bb8fd837865
Matrix <113445886@qq.com>
2020-04-24 11:50:50 +0800

    feat add format decimal test

--- lib/global/common/util.dart
+++ lib/global/common/util.dart
@@ -1,11 +1,12 @@
 import 'package:decimal/decimal.dart';
 
 String formatDecimal(Decimal decimal, int precision) {
+  assert(precision >= 0);
   final decimalStr = decimal.toString();
   final length = decimalStr.length;
   final indexOfDot = decimalStr.indexOf('.');
   final appendLength =
-      indexOfDot < 0 ? 2 : precision - (length - indexOfDot - 1);
+      indexOfDot < 0 ? precision : precision - (length - indexOfDot - 1);
 
   if (appendLength >= 0) {
     return decimal.toStringAsFixed(precision);
