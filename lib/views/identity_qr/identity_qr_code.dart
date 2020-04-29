0842b7ce4564e99b04f4ec45589b2fd537699e1c
xudongdong <dongdong.xu1@thoughtworks.com>
2020-04-29 16:46:44 +0800

    feat: finish 当用户选择“出示二维码”，用户可以看到二维码，包含的信息：DID (AC2) #60

--- lib/views/identity_qr/identity_qr_code.dart
+++ lib/views/identity_qr/identity_qr_code.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:tw_wallet_ui/global/widgets/layouts/common_layout.dart';
 import 'package:tw_wallet_ui/models/identity.dart';
 
 class IdentityQRPage extends StatelessWidget {
@@ -7,6 +9,34 @@ class IdentityQRPage extends StatelessWidget {
     final Identity identity = ModalRoute.of(context).settings.arguments;
     print(identity);
 
-    return Scaffold();
+    return CommonLayout(child: _buildMainContent(identity), title: '身份二维码',);
+  }
+
+  Widget _buildMainContent(Identity identity) {
+    return
+      Container(
+        padding: EdgeInsets.only(top: 88, left: 28, right: 28),
+        child: Column(
+          children: <Widget>[
+            Text(identity.name,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+            _buildQRCode(identity.did),
+            Text(identity.did,
+              style: TextStyle(fontSize: 18, color: Color(0xff888888)),),
+          ],
+        ),
+      );
+  }
+
+  Widget _buildQRCode(String did) {
+    return Container(
+      margin: EdgeInsets.all(44),
+      child:
+      QrImage(
+        data: did,
+        version: QrVersions.auto,
+//        size: 200.0,
+      ),
+    );
   }
 }
