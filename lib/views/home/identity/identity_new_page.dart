05e952b3930f22221e2e60427abbfc5df289abfd
Matrix <113445886@qq.com>
2020-05-07 13:38:23 +0800

    chore optimization identity new page

--- lib/views/home/identity/identity_new_page.dart
+++ lib/views/home/identity/identity_new_page.dart
@@ -46,89 +46,92 @@ class _IdentityNewPageState extends State<IdentityNewPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: WalletTheme.mainBgColor,
-      body: SafeArea(
-          child: Container(
-              color: WalletTheme.mainBgColor,
-              child: Column(children: <Widget>[
-                Container(
-                    padding: EdgeInsets.all(25),
-                    child: PageTitleWidget(title: '新建个人信息')),
-                Expanded(
-                    child: Form(
-                  child: Padding(
-                      padding: EdgeInsets.all(30),
-                      child: Column(
-                        children: <Widget>[
-                          Observer(
-                              builder: (_) => Stack(
-                                      alignment: const Alignment(0.0, 2.0),
-                                      children: <Widget>[
-                                        AvataaarImage(
-                                          avatar: store.avatar,
-                                          errorImage: Icon(Icons.error),
-                                          placeholder:
-                                              CircularProgressIndicator(),
-                                          width: 65,
-                                        ),
-                                        IconButton(
-                                            icon: Icon(Icons.refresh),
-                                            onPressed: store.refreshAvatar)
-                                      ])),
-                          Padding(
-                              padding: EdgeInsets.all(15),
-                              child: Text('以下带 * 的为必填项',
-                                  style: TextStyle(color: Colors.grey))),
-                          Observer(
-                              builder: (_) => TextField(
-                                  keyboardType: TextInputType.text,
-                                  onChanged: (value) => store.name = value,
-                                  decoration: InputDecoration(
-                                    labelText: '名称*',
-                                    hintText: '输入名称',
-                                    errorText: store.error.username,
-                                  ))),
-                          Observer(
-                              builder: (_) => TextField(
-                                  onChanged: (value) => store.email = value,
-                                  keyboardType: TextInputType.emailAddress,
-                                  decoration: InputDecoration(
-                                    labelText: '邮箱',
-                                    hintText: '输入邮箱',
-                                    errorText: store.error.email,
-                                  ))),
-                          Observer(
-                              builder: (_) => TextField(
-                                  onChanged: (value) => store.phone = value,
-                                  keyboardType: TextInputType.phone,
-                                  decoration: InputDecoration(
-                                    labelText: '手机',
-                                    hintText: '输入手机号',
-                                    errorText: store.error.phone,
-                                  ))),
-                          Observer(
-                              builder: (_) => TextField(
-                                  onChanged: (value) => store.birthday = value,
-                                  keyboardType: TextInputType.datetime,
-                                  decoration: InputDecoration(
-                                    labelText: '生日',
-                                    hintText: 'YYYY-MM-DD',
-                                    errorText: store.error.birthday,
-                                  ))),
-                          Expanded(child: Container()),
-                          Padding(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 30, vertical: 55),
-                              child: Observer(
-                                  builder: (_) => WalletTheme.button(
-                                      text: '添加',
-                                      onPressed: store.error.hasErrors
-                                          ? null
-                                          : _addOnPressed)))
-                        ],
-                      )),
-                ))
-              ]))),
       resizeToAvoidBottomPadding: false,
+      body: SafeArea(
+          child: LayoutBuilder(
+        builder: (context, constraints) => Container(
+            color: WalletTheme.mainBgColor,
+            child: Column(children: <Widget>[
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: PageTitleWidget(title: '新建个人信息')),
+              Expanded(
+                  child: Form(
+                child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25),
+                    child: Column(
+                      children: <Widget>[
+                        Observer(
+                            builder: (_) => Stack(
+                                    alignment: const Alignment(0.0, 2.0),
+                                    children: <Widget>[
+                                      AvataaarImage(
+                                        avatar: store.avatar,
+                                        errorImage: Icon(Icons.error),
+                                        placeholder:
+                                            CircularProgressIndicator(),
+                                        width: 65,
+                                      ),
+                                      IconButton(
+                                          icon: Icon(Icons.refresh),
+                                          onPressed: store.refreshAvatar)
+                                    ])),
+                        Padding(
+                            padding: EdgeInsets.only(top: 15),
+                            child: Text('以下带 * 的为必填项',
+                                style: TextStyle(color: Colors.grey))),
+                        Observer(
+                            builder: (_) => TextField(
+                                keyboardType: TextInputType.text,
+                                onChanged: (value) => store.name = value,
+                                decoration: InputDecoration(
+                                  labelText: '名称*',
+                                  hintText: '输入名称',
+                                  errorText: store.error.username,
+                                ))),
+                        Observer(
+                            builder: (_) => TextField(
+                                onChanged: (value) => store.email = value,
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: InputDecoration(
+                                  labelText: '邮箱',
+                                  hintText: '输入邮箱',
+                                  errorText: store.error.email,
+                                ))),
+                        Observer(
+                            builder: (_) => TextField(
+                                onChanged: (value) => store.phone = value,
+                                keyboardType: TextInputType.phone,
+                                decoration: InputDecoration(
+                                  labelText: '手机',
+                                  hintText: '输入手机号',
+                                  errorText: store.error.phone,
+                                ))),
+                        Observer(
+                            builder: (_) => TextField(
+                                onChanged: (value) => store.birthday = value,
+                                keyboardType: TextInputType.datetime,
+                                decoration: InputDecoration(
+                                  labelText: '生日',
+                                  hintText: 'YYYY-MM-DD',
+                                  errorText: store.error.birthday,
+                                ))),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 25,
+                              vertical: 20,
+                            ),
+                            child: Observer(
+                                builder: (_) => WalletTheme.button(
+                                    text: '添加',
+                                    onPressed: store.error.hasErrors
+                                        ? null
+                                        : _addOnPressed)))
+                      ],
+                    )),
+              ))
+            ])),
+      )),
     );
   }
 }
