6cd4032f1c43199174d033d73858459b2a6a6b7d
xudongdong <dongdong.xu1@thoughtworks.com>
2020-05-07 17:17:39 +0800

    feature: finish health code page UI #9

--- lib/views/health_code/health_code.dart
+++ lib/views/health_code/health_code.dart
@@ -14,66 +14,56 @@ class HealthCodeState extends State<HealthCodePage> {
   @override
   Widget build(BuildContext context) {
     return CommonLayout(
-      title: '健康码',
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 29),
-        child: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsetsDirectional.only(top: 50),
-              child: Center(
-                child: Text(
-                  '小钱',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: WalletTheme.rgbColor('#222222'),
-                    height: 1.6
-                  )
+        title: '健康码',
+        child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 49),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsetsDirectional.only(top: 50),
+                  child: Center(
+                    child: Text('小钱',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: WalletTheme.rgbColor('#222222'),
+                            height: 1.6)),
+                  ),
                 ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsetsDirectional.only(top: 50),
-              child: QrImage(
-                data: 'health_status',
-                version: QrVersions.auto,
-                size: 200,
-                foregroundColor: Colors.green
-              ),
-            ),
-            Container(
-              width: 320,
-              margin: EdgeInsetsDirectional.only(top: 50),
-              child: Center(
-                child: Text(
-                  '绿码：截止到当前，该身份的持有者没有暴露在病毒污染的环境中。',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    letterSpacing: 1.4,
-                    color: WalletTheme.rgbColor('#888888'),
-                    height: 1.43
+                _buildQrImage("1231111111111111111111111111111", false),
+                Center(
+                  child: Text(
+                    '绿码：截止到当前，该身份的持有者没有暴露在病毒污染的环境中。',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        letterSpacing: 1.4,
+                        color: WalletTheme.rgbColor('#888888'),
+                        height: 1.43),
                   ),
+                ),
+                Container(
+                  margin: EdgeInsetsDirectional.only(top: 20),
+                  child: Center(
+                      child: Text(
+                        '红码：该身份的持有者有病毒污染暴露风险。',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            letterSpacing: 1.4,
+                            color: WalletTheme.rgbColor('#888888'),
+                            height: 1.43),
+                      )),
                 )
-              ),
-            ),
-            Container(
-              width: 320,
-              margin: EdgeInsetsDirectional.only(top: 20),
-              child: Center(
-                child: Text(
-                  '红码：该身份的持有者有病毒污染暴露风险。',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    letterSpacing: 1.4,
-                    color: WalletTheme.rgbColor('#888888'),
-                    height: 1.43
-                  ),
-                )
-              ),
-            )
-          ],
-        )
-      )
+              ],
+            )));
+  }
+
+  Widget _buildQrImage(String data, bool isHealth) {
+    final color = isHealth ? Colors.green : Colors.red;
+    return Container(
+      margin: EdgeInsets.all(50),
+      decoration: BoxDecoration(
+          border: Border.all(color: color, width: 5)
+      ),
+      child: QrImage(data: data, size: 200, foregroundColor: color),
     );
   }
 }
