86ab36237c155ffeb21e8b2a7e0e2b949117bac1
zyzhao <zyzhao@thoughtworks.com>
2020-05-21 15:47:02 +0800

    fix: new wallet page style

--- lib/views/new_wallet/new_wallet_widget.dart
+++ lib/views/new_wallet/new_wallet_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tw_wallet_ui/common/application.dart';
 import 'package:tw_wallet_ui/common/theme/color.dart';
 import 'package:tw_wallet_ui/common/theme/font.dart';
@@ -8,6 +9,41 @@ import 'package:tw_wallet_ui/widgets/layouts/new_common_layout.dart';
 import 'package:tw_wallet_ui/widgets/page_title.dart';
 
 class NewWalletWidget extends StatelessWidget {
+
+  Widget buildBigButton({String title, bool disabled = false}) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric(vertical: 70),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+        color: WalletColor.lightGrey,
+      ),
+      child: Center(
+        child: Stack(
+          overflow: Overflow.visible,
+          children: <Widget>[
+            Text(
+              title,
+              style: WalletFont.font_24(
+                textStyle: TextStyle(
+                  color: disabled ? WalletColor.grey : WalletColor.primary
+                )
+              )
+            ),
+            Positioned(
+              child: SvgPicture.asset(
+                'assets/icons/new-wallet.svg',
+                color: disabled ? WalletColor.grey : WalletColor.primary
+              ),
+              top: -7,
+              left: -76
+            )
+          ],
+        )
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return NewCommonLayout(
@@ -35,53 +71,11 @@ class NewWalletWidget extends StatelessWidget {
               ),
               GestureDetector(
                 onTap: () => Application.router.navigateTo(context, Routes.backupMnemonics),
-                child: Container(
-                  margin: EdgeInsets.only(top: 40),
-                  padding: EdgeInsets.symmetric(horizontal: 46, vertical: 70),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                    color: WalletColor.lightGrey,
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Image(image: AssetImage('assets/images/new-wallet.png')),
-                      Container(
-                        margin: EdgeInsets.only(left: 40),
-                        child: Text(
-                          '创建钱包',
-                          style: WalletFont.font_24()
-                        )
-                      )
-                    ],
-                  ),
-                ),
+                child: buildBigButton(title: '创建钱包'),
               ),
               GestureDetector(
                 onTap: null,
-                child: Container(
-                  margin: EdgeInsets.only(top: 20),
-                  padding: EdgeInsets.symmetric(horizontal: 46, vertical: 70),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                    color: WalletColor.lightGrey,
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Image(image: AssetImage('assets/images/new-wallet.png')),
-                      Container(
-                        margin: EdgeInsets.only(left: 40),
-                        child: Text(
-                          '恢复钱包',
-                          style: WalletFont.font_24(
-                            textStyle: TextStyle(
-                              color: WalletColor.grey,
-                            )
-                          )
-                        )
-                      )
-                    ],
-                  ),
-                ),
+                child: buildBigButton(title: '恢复钱包', disabled: true),
               ),
             ],
           )
