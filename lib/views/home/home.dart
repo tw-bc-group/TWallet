ca593dfad908c86578a175c4eb87447ca9793bcd
xudongdong <dongdong.xu1@thoughtworks.com>
2020-05-21 17:19:37 +0800

    feature: change bottom bar icons #103

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_dialog/flutter_custom_dialog.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tw_wallet_ui/common/http/error_interceptor.dart';
 import 'package:tw_wallet_ui/common/theme/color.dart';
 import 'package:tw_wallet_ui/views/home/home_store.dart';
@@ -13,6 +14,7 @@ import 'my/my_page.dart';
 
 class Home extends StatefulWidget {
   Home({this.defaultIndex = 0});
+
   final int defaultIndex;
 
   @override
@@ -23,11 +25,45 @@ class HomeState extends State<Home> {
   HomeState({this.defaultIndex = 0});
 
   final HomeStore homeStore = HomeStore();
+
+  static final iconPaths = {
+    'home': {
+      'unselected': 'assets/icons/bottom_bar/home.svg',
+      'selected': 'assets/icons/bottom_bar/home_selected.svg',
+    },
+    'discovery': {
+      'unselected': 'assets/icons/bottom_bar/discovery.svg',
+      'selected': 'assets/icons/bottom_bar/discovery_selected.svg',
+    },
+    'identity': {
+      'unselected': 'assets/icons/bottom_bar/identity.svg',
+      'selected': 'assets/icons/bottom_bar/identity_selected.svg',
+    },
+    'me': {
+      'unselected': 'assets/icons/bottom_bar/me.svg',
+      'selected': 'assets/icons/bottom_bar/me_selected.svg',
+    }
+  };
+
+  static svgIcon(path) => SvgPicture.asset(path);
+
   static final List<BottomNavigationBarItem> _barItems = [
-    BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('首页')),
-    BottomNavigationBarItem(icon: Icon(Icons.more), title: Text('发现')),
-    BottomNavigationBarItem(icon: Icon(Icons.business), title: Text('身份')),
-    BottomNavigationBarItem(icon: Icon(Icons.account_box), title: Text('我')),
+    BottomNavigationBarItem(
+        icon: svgIcon(iconPaths['home']['unselected']),
+        activeIcon: svgIcon(iconPaths['home']['selected']),
+        title: Text('首页')),
+    BottomNavigationBarItem(
+        icon: svgIcon(iconPaths['discovery']['unselected']),
+        activeIcon: svgIcon(iconPaths['discovery']['selected']),
+        title: Text('发现')),
+    BottomNavigationBarItem(
+        icon: svgIcon(iconPaths['identity']['unselected']),
+        activeIcon: svgIcon(iconPaths['identity']['selected']),
+        title: Text('身份')),
+    BottomNavigationBarItem(
+        icon: svgIcon(iconPaths['me']['unselected']),
+        activeIcon: svgIcon(iconPaths['me']['selected']),
+        title: Text('我')),
   ];
 
   static int get identityIndex => 2;
