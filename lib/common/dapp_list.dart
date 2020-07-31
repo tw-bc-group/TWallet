fc09eeb849204ab68ec245c6eb7ecd1cd24f16a3
zyzhao <zyzhao@thoughtworks.com>
2020-07-31 15:48:59 +0800

    fix: revert dapp id wording

--- lib/common/dapp_list.dart
+++ lib/common/dapp_list.dart
@@ -8,50 +8,50 @@ class DAppInfo {
 }
 
 List<DAppInfo> dappList = [
-  DAppInfo('club-local', 'M. CLUB LOCAL', 'http://127.0.0.1:3000',
+  DAppInfo('dapp-1', 'M. CLUB LOCAL', 'http://127.0.0.1:3000',
       'assets/icons/dapp/loyalty-club.svg'),
   DAppInfo(
-      'club-dev',
+      'dapp-2',
       'M. CLUB DEV',
       'https://tw-loyalty-club-app-dev.s3.us-east-2.amazonaws.com/www/index.html',
       'assets/icons/dapp/loyalty-club.svg'),
   DAppInfo(
-      'club-qa',
+      'dapp-3',
       'M. CLUB QA',
       'https://tw-loyalty-club-app-qa.s3.us-east-2.amazonaws.com/www/index.html',
       'assets/icons/dapp/loyalty-club.svg'),
   DAppInfo(
-      'club-uat',
+      'dapp-7',
       'M. CLUB UAT',
       'https://tw-loyalty-club-app-uat.s3.us-east-2.amazonaws.com/www/index.html',
       'assets/icons/dapp/loyalty-club.svg'),
   DAppInfo(
-      'club-prod',
+      'dapp-9',
       'M. CLUB',
       'https://tw-loyalty-club-app-prod.s3.us-east-2.amazonaws.com/www/index.html',
       'assets/icons/dapp/loyalty-club.svg'),
   DAppInfo(
-      'enterprise-local',
+      'dapp-4',
       'M. Enterprise LOCAL',
       'http://127.0.0.1:3000/#/enterprise',
       'assets/icons/dapp/loyalty-enterprise.svg'),
   DAppInfo(
-      'enterprise-dev',
+      'dapp-5',
       'M. Enterprise DEV',
       'https://tw-loyalty-club-app-dev.s3.us-east-2.amazonaws.com/www/index.html#/enterprise',
       'assets/icons/dapp/loyalty-enterprise.svg'),
   DAppInfo(
-      'enterprise-qa',
+      'dapp-6',
       'M. Enterprise QA',
       'https://tw-loyalty-club-app-qa.s3.us-east-2.amazonaws.com/www/index.html#/enterprise',
       'assets/icons/dapp/loyalty-enterprise.svg'),
   DAppInfo(
-      'enterprise-uat',
+      'dapp-8',
       'M. Enterprise UAT',
       'https://tw-loyalty-club-app-uat.s3.us-east-2.amazonaws.com/www/index.html#/enterprise',
       'assets/icons/dapp/loyalty-enterprise.svg'),
   DAppInfo(
-      'enterprise-prod',
+      'dapp-10',
       'M. Enterprise',
       'https://tw-loyalty-club-app-prod.s3.us-east-2.amazonaws.com/www/index.html#/enterprise',
       'assets/icons/dapp/loyalty-enterprise.svg')
