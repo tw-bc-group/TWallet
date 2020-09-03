210e459a788f057dd19cc4ef076df3aa7c324af5
Matrix <113445886@qq.com>
2020-09-03 17:07:45 +0800

    chore: format code

--- test/amount_test.dart
+++ test/amount_test.dart
@@ -7,28 +7,19 @@ void main() {
 
   group('Amount', () {
     test('Amount human readable should be 1.00', () {
-      expect(Amount
-          .parse('100')
-          .humanReadable, '1.00');
+      expect(Amount.parse('100').humanReadable, '1.00');
     });
 
     test('Amount human readable should be -1.00', () {
-      expect(Amount
-          .parse('-100')
-          .humanReadable, '-1.00');
+      expect(Amount.parse('-100').humanReadable, '-1.00');
     });
 
     test('Amount human readable with flag should be +1.00', () {
-      expect(Amount
-          .parse('100')
-          .humanReadableWithSign, '+￥1.00');
+      expect(Amount.parse('100').humanReadableWithSign, '+￥1.00');
     });
 
     test('Amount human readable with flag should be -1.00', () {
-      expect(Amount
-          .parse('-100')
-          .humanReadableWithSign, '-￥1.00');
+      expect(Amount.parse('-100').humanReadableWithSign, '-￥1.00');
     });
   });
-
 }
