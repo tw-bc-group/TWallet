0c21e1630ed86ccfb3038f9fc113c9bbb2c4dab1
Weili Yi <wlyi@thoughtworks.com>
2021-01-12 11:01:21 +0800

    feat: Add countdown to qr code.

--- lib/views/ssi/pass_page.dart
+++ lib/views/ssi/pass_page.dart
@@ -1,8 +1,10 @@
+import 'package:corsac_jwt/corsac_jwt.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tw_wallet_ui/models/vc_pass.dart';
 import 'package:tw_wallet_ui/store/vc_store.dart';
+import 'package:tw_wallet_ui/widgets/error_row.dart';
 import 'package:tw_wallet_ui/widgets/header.dart';
 import 'package:tw_wallet_ui/widgets/layouts/common_layout.dart';
 import 'package:tw_wallet_ui/widgets/qr_card.dart';
@@ -16,11 +18,18 @@ class PassPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    JWT jwt = JWT.parse(vcPass.token);
+    try {
+      final exp = jwt.getClaim("exp") as int;
+      print(exp);
+      return CommonLayout(
+          child: Column(
+              children: <Widget>[
+                Header(title: "请出示用于\n【${vcPass.name}】"),
+                QrCard(data: vcPass.token, before: DateTime.fromMillisecondsSinceEpoch(exp*1000))]));
+    } catch (e){
+      return ErrorRowWidget(errorText: "$e");
+    }
 
-    return CommonLayout(
-        child: Column(
-            children: <Widget>[
-              Header(title: "请出示用于\n【${vcPass.name}】"),
-              QrCard(data: vcPass.token,)]));
   }
 }
--- lib/widgets/qr_card.dart
+++ lib/widgets/qr_card.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:intl/intl.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:tw_wallet_ui/common/theme/color.dart';
+import 'package:tw_wallet_ui/widgets/countdown_timer.dart';
+import 'package:tw_wallet_ui/widgets/error_row.dart';
 
-class QrCard extends StatelessWidget {
-  final String data;
-  final int remainSeconds;
-
+class QrCard extends StatefulWidget {
   const QrCard({
+    Key key,
     @required this.data,
-    this.remainSeconds
-  });
+    this.before,
+  }) : super(key: key);
+
+  final String data;
+  final DateTime before;
+
+  @override
+  _QrCardState createState() => _QrCardState();
+}
+
+class _QrCardState extends State<QrCard> {
+
+  bool isExpired = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void handleAtExpiration() {
+    setState(() {
+      isExpired = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final DateTime before = widget.before;
+
     return Container(
       decoration: BoxDecoration(
           borderRadius: const BorderRadius.all(Radius.circular(12)),
@@ -22,8 +47,10 @@ class QrCard extends StatelessWidget {
       margin: const EdgeInsets.only(left: 18, right: 18, top: 18),
       padding: const EdgeInsets.only(top: 60, bottom: 60, left: 60, right: 60),
       child: Column(children: [
-        QrImage(data: data),
-        if (remainSeconds != null) Text("${remainSeconds}s")]),
+        QrImage(data: widget.data, foregroundColor: isExpired ? WalletColor.red : WalletColor.black),
+        if (before !=null && !isExpired) CountdownTimer(before: before, atExpiration: handleAtExpiration),
+        if (before !=null && isExpired) ErrorRowWidget(errorText: "已于${DateFormat('yyyy-MM-dd kk:mm').format(before)}过期")
+      ]),
     );
   }
 }
