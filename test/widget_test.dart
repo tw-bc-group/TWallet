3c37c3c8270827663468f1b0ae9b399f75df1938
Yan Qian <qianyan@thoughtworks.com>
2022-08-09 10:31:21 +0800

    test: add new smoke test for inputPin page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,30 +5,20 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tw_wallet_ui/main.dart';
+import 'package:tw_wallet_ui/router/routers.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Landing page smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(
       TWallet(
-        initialRoute: '',
+        initialRoute: Routes.inputPin,
       ),
     );
 
-    // Verify that our counter starts at 0.
-//    expect(find.text('0'), findsOneWidget);
-//    expect(find.text('1'), findsNothing);
-//
-//    // Tap the '+' icon and trigger a frame.
-//    await tester.tap(find.byIcon(Icons.add));
-//    await tester.pump();
-//
-//    // Verify that our counter has incremented.
-//    expect(find.text('0'), findsNothing);
-//    expect(find.text('1'), findsOneWidget);
+    expect(find.text('请创建您的 PIN 码'), findsOneWidget);
   });
 }
