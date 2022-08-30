454fb96bb8880a1ebd7876f11dc4c83c6382e77c
Yan Qian <qianyan@thoughtworks.com>
2022-08-30 18:44:18 +0800

    refactor: widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,17 +9,14 @@ import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
-import 'package:magic_sdk/magic_sdk.dart';
 import 'package:mockito/annotations.dart';
 import 'package:tw_wallet_ui/common/secure_storage.dart';
-
 import 'package:tw_wallet_ui/main.dart';
 import 'package:tw_wallet_ui/router/routers.dart';
-import 'package:tw_wallet_ui/store/magic_link.dart';
 import 'package:tw_wallet_ui/views/input_pin/input_pin_widget.dart';
 
 import 'observer_tester.dart';
-
+// generated code which mocks the SecureStorage class
 import 'widget_test.mocks.dart';
 
 // Annotation which generates the cat.mocks.dart library and the MockCat class.
@@ -38,55 +35,14 @@ void main() {
 
   testWidgets('input pin smoke test', (tester) async {
     final List<NavigatorObservation> observations = <NavigatorObservation>[];
+    await inputPinsHaveBeenFilled(tester, observations);
+    await findNextStepButton(tester, '下一步');
 
-    final TestObserver observer = TestObserver()
-      ..onPushed = (Route<dynamic>? route, Route<dynamic>? previousRoute) {
-        // Pushes the initial route.
-        observations.add(
-          NavigatorObservation(
-            current: route?.settings.name,
-            previous: previousRoute?.settings.name,
-            operation: 'push',
-          ),
-        );
-      };
-
-    final ctl1 = TextEditingController();
-    final ctl2 = TextEditingController();
-
-    final Handler mockHandler = Handler(
-      handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
-        return PinInputWidget(ctl1, ctl2);
-      },
-    );
-    Routes.routers[Routes.inputPin] = mockHandler;
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(
-      TWallet(
-        initialRoute: Routes.inputPin,
-        navigatorObservers: [observer],
-      ),
-    );
-
-    ctl1.text = '123456';
-    ctl2.text = '123456';
-
-    await tester.pump();
-    final list = find.byType(ListView);
-    final scrollable = find.byWidgetPredicate((w) => w is Scrollable);
-    final scrollableOfList = find.descendant(of: list, matching: scrollable);
-
-    await tester.scrollUntilVisible(
-      find.text('下一步'),
-      200.0,
-      scrollable: scrollableOfList.first,
-    );
-
-    expect(find.text('下一步'), findsOneWidget);
-
+    // dummy secure storage
     Get.put<SecureStorage>(MockSecureStorage());
-    Get.put<Magic>(magicLink());
+
     await tester.tap(find.text('下一步'));
+
     await tester.pump();
 
     expect(observations.length, 3);
@@ -94,4 +50,54 @@ void main() {
     expect(observations[2].current, Routes.newWallet);
     expect(observations[2].previous, Routes.inputPin);
   });
+}
+
+Future<void> findNextStepButton(WidgetTester tester, String nextStep) async {
+  await tester.pump();
+  final list = find.byType(ListView);
+  final scrollable = find.byWidgetPredicate((w) => w is Scrollable);
+  final scrollableOfList = find.descendant(of: list, matching: scrollable);
+
+  await tester.scrollUntilVisible(
+    find.text(nextStep),
+    200.0,
+    scrollable: scrollableOfList.first,
+  );
+
+  expect(find.text(nextStep), findsOneWidget);
+}
+
+Future<void> inputPinsHaveBeenFilled(
+  WidgetTester tester,
+  List<NavigatorObservation> observations,
+) async {
+  final TestObserver observer = TestObserver()
+    ..onPushed = (Route<dynamic>? route, Route<dynamic>? previousRoute) {
+      // Pushes the initial route.
+      observations.add(
+        NavigatorObservation(
+          current: route?.settings.name,
+          previous: previousRoute?.settings.name,
+          operation: 'push',
+        ),
+      );
+    };
+  final ctl1 = TextEditingController();
+  final ctl2 = TextEditingController();
+  final Handler injectedHandler = Handler(
+    handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+      return PinInputWidget(ctl1, ctl2);
+    },
+  );
+  Routes.routers[Routes.inputPin] = injectedHandler;
+  // Build our app and trigger a frame.
+  await tester.pumpWidget(
+    TWallet(
+      initialRoute: Routes.inputPin,
+      navigatorObservers: [observer],
+    ),
+  );
+
+  ctl1.text = '123456';
+  ctl2.text = '123456';
 }
