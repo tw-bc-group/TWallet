2de2541dbb9cf089ffdbea9c36465e0a116680d1
Yan Qian <qianyan@thoughtworks.com>
2022-09-01 21:50:33 +0800

    chore: disable web3auth & magiclink

--- lib/common/getx.dart
+++ lib/common/getx.dart
@@ -12,27 +12,25 @@ import 'package:tw_wallet_ui/store/dcep/dcep_store.dart';
 import 'package:tw_wallet_ui/store/health_certification_store.dart';
 import 'package:tw_wallet_ui/store/identity_store.dart';
 import 'package:tw_wallet_ui/store/issuer_store.dart';
-import 'package:tw_wallet_ui/store/magic_link.dart';
 import 'package:tw_wallet_ui/store/mnemonics.dart';
 import 'package:tw_wallet_ui/store/vc_store.dart';
-import 'package:tw_wallet_ui/store/web3auth_store.dart';
 import 'package:tw_wallet_ui/views/ble_payment/common/tx_store.dart';
 
 Future<void> initGlobalDependencies() async {
   Get.put(SecureStorage());
   Get.put(ProgressDialog());
   Get.put(LoadingInterceptor());
-  Get.put(magicLink());
   Get.put(LogInterceptor(requestBody: true, responseBody: true));
   Get.put(HttpClient());
   Get.put(ApiProvider());
-  Get.put(web3authInit());
+  // Get.put(magicLink());
+  // Get.put(web3authInit());
   Get.put(
     JsonStore(dbName: identityStorageName),
     tag: identityStorageName,
   );
   await Get.putAsync(MnemonicsStore.init);
-  await Get.putAsync(MagicLinkStore.init);
+  // await Get.putAsync(MagicLinkStore.init);
   await Get.putAsync(ContractService.init);
   await Get.putAsync(IdentityStore.init);
   Get.put(DcepStore());
