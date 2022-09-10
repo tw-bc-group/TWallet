b8ac7c88e26e6f1702a9068b1fc0ff91286f85e4
Haoran Lin <tdkgn@outlook.com>
2022-09-10 17:48:44 +0800

    feat: localize home page

--- lib/generated/intl/messages_zh_CN.dart
+++ lib/generated/intl/messages_zh_CN.dart
@@ -21,5 +21,11 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'zh_CN';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{};
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "pageHomeAsset": MessageLookupByLibrary.simpleMessage("资产"),
+        "pageHomeCertification": MessageLookupByLibrary.simpleMessage("证书"),
+        "pageHomeChangeIdentity": MessageLookupByLibrary.simpleMessage("切换身份"),
+        "pageHomeDeed": MessageLookupByLibrary.simpleMessage("房产证"),
+        "pageHomeTicket": MessageLookupByLibrary.simpleMessage("票券")
+      };
 }
